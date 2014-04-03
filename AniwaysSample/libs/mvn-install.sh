f23c6795f27f1bd7952688aff37f5baa24e5467d
Francois Denommee <francois.denommee@jestadigital.com>
2014-04-03 13:56:19 +0200

    Updated to v1.78 (resolved corporate WiFi issue)

--- AniwaysSample/libs/mvn-install.sh
+++ AniwaysSample/libs/mvn-install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn install:install-file -Dfile=aniways-V1.76.jar -DgroupId=com.aniways -DartifactId=library -Dversion=1.76 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
+mvn install:install-file -Dfile=aniways-V1.78.jar -DgroupId=com.aniways -DartifactId=library -Dversion=1.78 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
 mvn install:install-file -Dfile=libGoogleAnalyticsV2.jar -DgroupId=com.google.analytics -DartifactId=tracking-android -Dversion=v2 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
